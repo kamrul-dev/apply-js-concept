05c415c7bf5fe33fad07fcb8138e543ff82a150d
Kamrul Hasan <you@example.com>
2022-01-30 00:05:29 +0600

    debug done.

---
+++ debug.js
@@ -0,0 +1,24 @@
+
+// find the error
+
+// function getFactorial(number){
+//     let factorial = 1;
+//     for(let i = 0; i<=number; i++){
+//         factorial = factorial * i;
+//     }
+//     return factorial;
+// }
+// const getMyFactorial = getFactorial(8);
+// console.log(getMyFactorial);
+
+
+
+// function getFactorial(number){
+//     let factorial = 1;
+//     for(let i = 1; i<=number; i++){
+//         factorial = factorial * i;
+//     }
+//     return factorial;
+// }
+// const getMyFactorial = getFactorial(8);
+// console.log(getMyFactorial);
