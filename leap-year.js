837393a34e48988803598ed61f0da43783488ae6
Kamrul Hasan <you@example.com>
2022-01-29 22:43:18 +0600

    Check whether a year is a Leap Year or not

---
+++ leap-year.js
@@ -0,0 +1,18 @@
+
+function isLeapYear(year){
+    if((year % 4 == 0) && (year % 100 != 0) || (year % 100 == 0)){     
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+const myYear = 2087;
+const isMyYearLeapYear = isLeapYear(myYear);
+console.log('Is my year leep year:', isMyYearLeapYear);
+
+
+const yourYearLeapYear = 2224;
+const isYourYearLeapYear = isLeapYear(yourYearLeapYear);
+console.log('is your year leap year: ', isYourYearLeapYear);
