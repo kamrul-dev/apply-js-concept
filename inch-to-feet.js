6b69475f9f2447fd1964f2dd1499c5bfe5f74d8d
Kamrul Hasan <you@example.com>
2022-01-29 21:49:02 +0600

    inches to feet and miles to kelometer

---
+++ inch-to-feet.js
@@ -0,0 +1,31 @@
+
+function inchToFeet(inches){
+
+    var feet = inches / 12;
+    return feet;
+}
+var myInches = 132;
+var feet = inchToFeet(myInches)
+console.log('myInches to feet',feet);
+
+var dadiInches = 144;
+var feet = inchToFeet(dadiInches)
+console.log('dadi inches to feet',feet)
+
+var naniInches = 156;
+var feet = inchToFeet(naniInches)
+console.log('naniInches to Feet',feet);
+
+var nanaInches = 168;
+var feet = inchToFeet(nanaInches);
+console.log('nanaInches to Feet',feet);
+
+
+// miles to kelometer
+
+function milesToKelometes(miles){
+    var km = miles * 1.60934;
+    return miles;
+}
+var marathon = milesToKelometes(26.2);
+console.log('Marathon miles in Kelometer',marathon);
