b3a4769a6c42a565df919b4315586c9c2636aa40
Kamrul Hasan <you@example.com>
2022-01-29 22:46:50 +0600

    leap year condition update

--- leap-year.js
+++ leap-year.js
@@ -1,6 +1,6 @@
 
 function isLeapYear(year){
-    if((year % 4 == 0) && (year % 100 != 0) || (year % 100 == 0)){     
+    if((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)){     
         return true;
     }
     else{
